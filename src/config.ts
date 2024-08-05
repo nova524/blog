0a78ef676c9ade0ec71901d380333e4e8fe993f1
Supernova <142123536+nova524@users.noreply.github.com>
2024-08-05 14:12:17 +0900

    ㅁㄴㅇㄹ

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Supernova',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  subtitle: '차승민',
+  lang: 'ko',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 95,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
